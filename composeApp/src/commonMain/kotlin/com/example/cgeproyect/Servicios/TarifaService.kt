d8a0c50108d0691815abe561ccd07db26a54eb97
Juan José Garrido <72580340+Copledot@users.noreply.github.com>
2025-11-09 13:05:09 -0300

    Update TarifaService.kt

--- composeApp/src/commonMain/kotlin/com/example/cgeproyect/Servicios/TarifaService.kt
+++ composeApp/src/commonMain/kotlin/com/example/cgeproyect/Servicios/TarifaService.kt
@@ -1,12 +1,10 @@
 package com.example.cgeproyect.servicios
 
-// --- IMPORTACIONES DEL DOMINIO ---
 import com.example.cgeproyect.Dominio.Cliente
 import com.example.cgeproyect.Dominio.Tarifa
 import com.example.cgeproyect.Dominio.TarifaComercial
 import com.example.cgeproyect.Dominio.TarifaResidencial
-
-// --- SERVICIO DE TARIFA ---
+-
 class TarifaService {
     fun tarifaParaCliente(cliente: Cliente): Tarifa {
         // Lee la propiedad guardada en el cliente
